b2d157ffd4dd19feb0bf04994fc65d29f5aafe9a
Vladyslav Bilyk <bilyk.vlad16@gmail.com>
2025-03-04 16:05:24 +0200

    Add generic Repository for entities

--- TelephoneConversations.Data/Repository/IRepository/ISubscriberRepository.cs
+++ TelephoneConversations.Data/Repository/IRepository/ISubscriberRepository.cs
@@ -1,15 +1,9 @@
-﻿using System.Linq.Expressions;
-using TelephoneConversations.Core.Models;
+﻿using TelephoneConversations.Core.Models;
 
 namespace TelephoneConversations.DataAccess.Repository.IRepository
 {
-    public interface ISubscriberRepository
+    public interface ISubscriberRepository : IRepository<Subscriber>
     {
-        Task<List<Subscriber>> GetAllAsync(Expression<Func<Subscriber, bool>> filter = null);
-        Task<Subscriber> GetAsync(Expression<Func<Subscriber, bool>> filter = null, bool tracked = true);
-        Task CreateAsync(Subscriber entity);
-        Task UpdateAsync(Subscriber entity);
-        Task RemoveAsync(Subscriber entity);
-        Task SaveAsync();
+        Task<Subscriber> UpdateAsync(Subscriber entity);
     }
 }
--- TelephoneConversations.Data/Repository/SubscriberRepository.cs
+++ TelephoneConversations.Data/Repository/SubscriberRepository.cs
@@ -1,69 +1,24 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System.Linq.Expressions;
-using TelephoneConversations.Core.Models;
+﻿using TelephoneConversations.Core.Models;
 using TelephoneConversations.DataAccess.Data;
 using TelephoneConversations.DataAccess.Repository.IRepository;
 
 namespace TelephoneConversations.DataAccess.Repository
 {
-    public class SubscriberRepository : ISubscriberRepository
+    public class SubscriberRepository : Repository<Subscriber>, ISubscriberRepository
     {
         private readonly ApplicationDbContext _db;
 
         public SubscriberRepository(ApplicationDbContext db)
+            : base(db)
         {
             _db = db;
         }
 
-        public async Task CreateAsync(Subscriber entity)
-        {
-            await _db.Subscribers.AddAsync(entity);
-            await SaveAsync();
-        }
-
-        public async Task<Subscriber> GetAsync(Expression<Func<Subscriber, bool>> filter = null, bool tracked = true)
-        {
-            IQueryable<Subscriber> query = _db.Subscribers;
-
-            if (!tracked)
-            {
-                query = query.AsNoTracking();
-            }
-            if (filter != null)
-            {
-                query = query.Where(filter);
-            }
-
-            return await query.FirstOrDefaultAsync();
-        }
-
-        public async Task<List<Subscriber>> GetAllAsync(Expression<Func<Subscriber, bool>> filter = null)
-        {
-            IQueryable<Subscriber> query = _db.Subscribers;
-
-            if (filter != null)
-            {
-                query = query.Where(filter);
-            }
-
-            return await query.ToListAsync();
-        }
-
-        public async Task RemoveAsync(Subscriber entity)
-        {
-            _db.Subscribers.Remove(entity);
-            await SaveAsync();
-        }
-
-        public async Task SaveAsync()
-        {
-            await _db.SaveChangesAsync();
-        }
-
-        public async Task UpdateAsync(Subscriber entity)
+        public async Task<Subscriber> UpdateAsync(Subscriber entity)
         {
             _db.Update(entity);
-            await SaveAsync();
+            await _db.SaveChangesAsync();
+            return entity;
         }
     }
 }
